345112336a11fd4991772d7adbafaffd41bb9313
Tautvydas Belgeras <t.belgeras@gmail.com>
2025-02-28 19:46:43 +0200

    Rename datetime field to avoid conflicts between its name and type

--- expense_tracker/routers/expenses/models.py
+++ expense_tracker/routers/expenses/models.py
@@ -7,4 +7,4 @@ class Expense(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)  # TODO don't allow use to pass id
     amount: float
     user_name: str  # TODO link to user model
-    datetime: datetime
+    timestamp: datetime
