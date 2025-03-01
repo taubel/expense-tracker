b17bfb43bcdeedeb0a98cf0341a11b0ec4715c0d
Tautvydas Belgeras <t.belgeras@gmail.com>
2025-03-01 18:47:47 +0200

    Separate data from db models. Add model validation to routers according to docs

--- expense_tracker/routers/expenses/models.py
+++ expense_tracker/routers/expenses/models.py
@@ -3,8 +3,19 @@ from datetime import datetime
 from sqlmodel import SQLModel, Field
 
 
-class Expense(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)  # TODO don't allow use to pass id
+class ExpenseBase(SQLModel):
     amount: float
     user_name: str  # TODO link to user model
     timestamp: datetime
+
+
+class Expense(ExpenseBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)  # TODO don't allow use to pass id
+
+
+class ExpenseCreate(ExpenseBase):
+    pass
+
+
+class ExpensePublic(ExpenseBase):
+    id: int
--- expense_tracker/routers/expenses/routes.py
+++ expense_tracker/routers/expenses/routes.py
@@ -2,29 +2,30 @@ from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 
 from ...dependencies import DBSessionDep
-from .models import Expense
+from .models import Expense, ExpenseCreate, ExpensePublic
 
 
 router = APIRouter(prefix="/expenses")
 
 
 # TODO add query parameters
-@router.get("/")
-async def get_all_expenses(session: DBSessionDep) -> list[Expense]:
+@router.get("/", response_model=list[ExpensePublic])
+async def get_all_expenses(session: DBSessionDep):
     expenses = session.exec(select(Expense))
     return expenses
 
 
-@router.get("/{item_id}")
-async def get_expense(item_id: int, session: DBSessionDep) -> Expense:
+@router.get("/{item_id}", response_model=ExpensePublic)
+async def get_expense(item_id: int, session: DBSessionDep):
     expense = session.get(Expense, item_id)
     if not expense:
         raise HTTPException(status_code=404, detail="Expense not found")
     return expense
 
 
-@router.post("/")
-async def add_expense(expense: Expense, session: DBSessionDep) -> Expense:
+@router.post("/", response_model=ExpensePublic)
+async def add_expense(expense: ExpenseCreate, session: DBSessionDep):
+    expense = Expense.model_validate(expense)
     session.add(expense)
     session.commit()
     session.refresh(expense)
