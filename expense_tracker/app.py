ecd802fe5544a3cf2cc529bfa3d85b5d39bfaecd
Tautvydas Belgeras <t.belgeras@gmail.com>
2025-03-06 21:16:11 +0200

    Include users router

--- expense_tracker/app.py
+++ expense_tracker/app.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from .dependencies import create_db_and_tables
-from .routers import expenses
+from .routers import expenses, users
 
 
 @asynccontextmanager
@@ -15,6 +15,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(expenses.router, prefix="/api")
+app.include_router(users.router, prefix="/api")
 
 @app.get("/")
 async def root():
